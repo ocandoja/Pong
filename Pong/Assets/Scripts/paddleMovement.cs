cd4f34620b01ba6087cfd1faa4f4b05188db3f38
José Ocando <jaogocando@gmail.com>
2021-02-04 12:55:32 -0500

    Background, Paddle and Ball
    Paddle movement and background position set.

---
+++ Pong/Assets/Scripts/paddleMovement.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class paddleMovement : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Debug.Log(Input.mousePosition);
+        Vector3 mousePosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        transform.position = new Vector3(transform.position.x, Mathf.Clamp(mousePosition.y, -3.89f, 3.89f), transform.position.z);
+    }
+}
